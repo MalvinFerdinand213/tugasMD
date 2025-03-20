fe47801cd62c725d5d70f1cbfb71e5d73c81d86d
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-03-20 21:15:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-# Set page title
 st.title("Machine Learning App")
+st.info("This app will predict your obesity level!")
 
-# Subtitle
-st.markdown("### This app will predict your obesity level!")
-
-# Load sample data (replace with your actual dataset)
-data = {
-    "Gender": ["Female", "Female", "Male", "Male", "Male", "Male", "Female", "Male", "Male", "Male"],
-    "Age": [21, 21, 23, 27, 22, 29, 23, 24, 21, 22],
-    "Height": [1.62, 1.52, 1.80, 1.87, 1.78, 1.62, 1.53, 1.78, 1.78, 1.72],
-    "Weight": [64, 56, 76, 87, 89.8, 53, 55, 85, 73, 68],
-    "family_history_with_overweight": ["yes", "yes", "yes", "no", "no", "yes", "yes", "yes", "yes", "yes"],
-    "FAVC": ["no", "no", "no", "no", "no", "yes", "no", "yes", "yes", "yes"],
-    "FCVC": [2, 3, 3, 2, 3, 3, 3, 3, 3, 3],
-    "NCP": [3, 3, 3, 2, 3, 3, 3, 3, 3, 3],
-    "CAEC": ["Sometimes", "Sometimes", "Sometimes", "Sometimes", "Sometimes", "Sometimes", "Sometimes", "Sometimes", "Sometimes", "Sometimes"]
-}
+data = pd.read_csv("ObesityDataSet_raw_and_data_sinthetic.csv") 
+st.write(data)
 
 df = pd.DataFrame(data)
 
