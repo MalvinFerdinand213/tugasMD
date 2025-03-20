56787d3b50ef5d15a68514390cbd23e9657280a6
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-03-20 21:45:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ fcvc = st.slider("Frequency of Vegetable Consumption", 1, 3, 2)
 ncp = st.slider("Number of Main Meals", 1, 4, 3)
 caec = st.selectbox("Consumption of Food Between Meals", ["Sometimes", "Frequently", "Always", "No"])
 """
-smoke = st.selectbox("Do you smoke?", ["yes", "no"])
+###smoke = st.selectbox("Do you smoke?", ["yes", "no"])
 ch2o = st.slider("Daily Water Intake (liters)", 1, 4, 2)
 scc = st.selectbox("Do you have high cholesterol?", ["yes", "no"])
 faf = st.slider("Physical Activity Frequency (days/week)", 0, 7, 3)
