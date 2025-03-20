3a579abe5b7032d76178b89112a29f2916580aaf
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-03-20 21:24:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,11 @@ data = pd.read_csv("ObesityDataSet_raw_and_data_sinthetic.csv")
 df = pd.DataFrame(data)
 
 # Expander to show raw data
-with st.expander("### Data"):
+with st.expander("Data"):
     st.write("This is a raw data")
     st.dataframe(df)
 
 #Data Visualilzation
 with st.expander("Data Visualization"):
-    st.write("### Data Visualization")
+    st.write("Data Visualization")
 
-    # Membuat scatter plot
-    fig = px.scatter(df, x="Height", y="Weight", color="Obesity_Type",
-                 title="Height vs Weight Distribution",
-                 labels={"Height": "Height", "Weight": "Weight"},
-                 color_discrete_map={
-                     "Insufficient_Weight": "blue",
-                     "Normal_Weight": "green",
-                     "Obesity_Type_I": "red",
-                     "Obesity_Type_II": "orange"
-                 })
-
-    st.plotly_chart(fig)
