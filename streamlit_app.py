fe69a3e75c7c35c47b4d87f42b403525d248764b
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-03-20 21:10:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv("ObesityDataSet_raw_and_data_sinthetic.csv") 
-st.write(df)
+st.info('This app will predict your obesite level!')
+
+def main():
+  haha = st.selectbox('Gender')
+  df = pd.read_csv("ObesityDataSet_raw_and_data_sinthetic.csv") 
+  st.write(df)
+
+if __name__ == "__main__":
+  main()
